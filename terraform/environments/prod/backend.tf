6dd9c8feca99a84f19337e3c94c5ba6cac7ca5d4
Maksim Partridge <Partridge@deeplay.io>
2025-11-12 03:24:54 +0100

    Fix prod backend S3 bucket name
    - Removed erroneous 'eu' from bucket name
    - Aligns with actual bucket created by bootstrap script
    - Same fix as dev environment

--- terraform/environments/prod/backend.tf
+++ terraform/environments/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-demo-flask-app-eu-851725636341"
+    bucket         = "terraform-state-demo-flask-app-851725636341"
     key            = "prod/terraform.tfstate"
     region         = "eu-north-1"
     dynamodb_table = "terraform-state-lock-demo-flask-app"
